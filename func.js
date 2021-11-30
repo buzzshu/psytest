5bb337706ecde386c9962ea142bce88959cfe377
buss <leozxc2009@gmail.com>
2021-11-30 19:11:31 +0800

    add psytest page

---
+++ func.js
@@ -0,0 +1,25 @@
+function onques(){
+    // basic information: display:none
+    document.getElementById("basicinfo").style.display="none"
+    // question: display:block 
+    document.getElementById("question").style.display="block"
+}
+function showFruit(){
+    // check last question is selected
+    if(document.querySelectorAll('input[class="ques"]:checked').length==4){
+                    // hidden form
+    document.getElementById("question").style.display="none"
+    // show div.result
+    document.getElementById("result").style.display="block"
+    // querySelector
+    // document.getElementById("result").innerHTML = document.querySelector('input[name="q4"]:checked').value
+    let fruit = document.querySelector('input[name="q4"]:checked').value
+    document.getElementById("result").innerHTML = "根據剛剛的心理測驗，由於你最喜歡的水果是" + fruit + "，你是個喜歡" + fruit + "的人。"
+
+
+
+    }else{
+        // warning
+        window.alert("還沒作答完唷！")
+    }
+}
